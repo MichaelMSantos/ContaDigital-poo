07c1675929c9c7419a127d8616bf3871f92d46d1
Michael Santos <mmss240610@gmail.com>
2025-07-30 21:13:14 -0300

    test: add method calls in main to manually test app

--- src/main/java/org/example/Banco.java
+++ src/main/java/org/example/Banco.java
@@ -1,12 +1,27 @@
 package org.example;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Getter @Setter
+@Data
 public class Banco {
     private String nome;
-    private List<Conta> conta;
+    private List<Conta> contas;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+    }
+
+    public void adicionarConta(Conta conta){
+        contas.add(conta);
+    }
+
+    public double getSaldoTotalBanco(){
+        return contas
+                .stream()
+                .mapToDouble(Conta::getSaldo)
+                .sum();
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,21 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Banco b = new Banco();
+        Cliente c = new Cliente();
+        c.setNome("Cliente 1");
+
+        Conta cc = new ContaCorrente(c);
+        Conta cp = new ContaPoupanca(c);
+        b.adicionarConta(cc);
+        b.adicionarConta(cp);
+
+        cc.depositar(20);
+        cc.transferir(2, cp);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+
+        System.out.println("=== Valor Total Depositado no Banco === \nR$: " + b.getSaldoTotalBanco());
     }
 }
